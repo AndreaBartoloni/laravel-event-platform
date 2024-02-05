db1937eed3dde2358f2086ad604aa4383d82144c
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2024-02-05 16:42:43 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+
+	Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+	
+	// Admin Post CRUD
+	//Route::resource('posts', PostController::class);
+});
 
 require __DIR__.'/auth.php';
